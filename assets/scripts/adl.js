ca794c0086770500a63ccf4074b1f2310ba97186
hifocus <i@focuschen.com>
2019-08-04 16:35:13 +0800

    Display user-langauge in footer to prevent confusion

--- assets/scripts/adl.js
+++ assets/scripts/adl.js
@@ -19,7 +19,7 @@ if (/zh-CN|zh-cn|zh-Hans|zh-hans|cn/i.test(userLang)) {
     document.getElementById("title").setAttribute("data-tooltip", "下载应用 于瞬息之间 ⚡");
     document.getElementById("howto").setAttribute("data-tooltip", "浏览文档");
     document.getElementById("supported").setAttribute("data-tooltip", "已支持 " + app_supported  + " 个应用");
-    document.getElementById("footer").innerHTML = "版本: " + "<span class='grey'>" + version +"</span> | 开源于 <a class='tooltip'id='dont' href='https://github.com/hifocus/adl'>GitHub</a>";
+    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>你的浏览器语言为 <code>" + userLang + "</code></span><br>版本: " + "<span class='grey'>" + version +"</span> | 开源于 <a class='tooltip'id='dont' href='https://github.com/hifocus/adl'>GitHub</a>";
     document.getElementById("dont").setAttribute("data-tooltip", "不要忘记 ⭐ 我们的项目");
 }
 else if (/zh-TW|zh-HK|zh-tw|zh-hk|zh-Hant|zh-hant|tw|hk/i.test(userLang)) {
@@ -32,7 +32,7 @@ else if (/zh-TW|zh-HK|zh-tw|zh-hk|zh-Hant|zh-hant|tw|hk/i.test(userLang)) {
     document.getElementById("howto").setAttribute("data-tooltip", "閱覽文檔");
     document.getElementById("supported").innerHTML = "已支援的應用";
     document.getElementById("supported").setAttribute("data-tooltip", "已支援 " + app_supported  + " 款應用");
-    document.getElementById("footer").innerHTML = "版本: " + "<span class='grey'>" + version +"</span> |  開源於 <a class='tooltip'id='dont' href='https://github.com/hifocus/adl'>GitHub</a>";
+    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>你的瀏覽器語言為 <code>" + userLang + "</code></span><br>版本: " + "<span class='grey'>" + version +"</span> |  開源於 <a class='tooltip'id='dont' href='https://github.com/hifocus/adl'>GitHub</a>";
     document.getElementById("dont").setAttribute("data-tooltip", "不要忘記 ⭐ 我們的項目");
 }
 else {
@@ -41,6 +41,6 @@ else {
     document.getElementById("title").setAttribute("data-tooltip", "Download apps in seconds ⚡");
     document.getElementById("howto").setAttribute("data-tooltip", "Browse docs");
     document.getElementById("supported").setAttribute("data-tooltip", app_supported  + " apps are supported");
-    document.getElementById("footer").innerHTML = "Version: " + version + " | Open Source Project on <a class='tooltip'id='dont' href='https://github.com/hifocus/adl'>GitHub</a>";
+    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>Your browser language is <code>" + userLang + "</code></span><br>Version: " + "<span class='grey'>" + version +"</span> | Open Source Project on <a class='tooltip'id='dont' href='https://github.com/hifocus/adl'>GitHub</a>";
     document.getElementById("dont").setAttribute("data-tooltip", "Don't forget to ⭐ our project!");
 }
