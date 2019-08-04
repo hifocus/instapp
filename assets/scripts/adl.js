1b2e4e354f41ace07406ea8f57fb7cd7ad454255
hifocus <i@focuschen.com>
2019-08-04 19:25:54 +0800

    Update wordings and translations

--- assets/scripts/adl.js
+++ assets/scripts/adl.js
@@ -6,7 +6,6 @@ if (/zh|cn|tw|hk|TW|HK/i.test(userLang)) {
     // detect browser langauge, both simplified chinese and traditional chinese
     // place translations that are the same in both form of chinese
     document.getElementById("discover").innerHTML = "更多";
-    document.getElementById("github").innerHTML = "GitHub 上的 ADL";
     document.getElementById("howto").innerHTML = "如何使用";
 }
 if (/zh-CN|zh-cn|zh-Hans|zh-hans|cn/i.test(userLang)) {
@@ -14,6 +13,7 @@ if (/zh-CN|zh-cn|zh-Hans|zh-hans|cn/i.test(userLang)) {
     document.write("<style>.lato { font-family: 'Lato', -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', sans-serif; }</style>");
     document.title = 'ADL - 下载应用 于瞬息之间';
     document.getElementById("docs").innerHTML = "浏览文档";
+    document.getElementById("github").innerHTML = "GitHub 页面";
     document.getElementById("blog").innerHTML = "惶心的博客";
     document.getElementById("supported").innerHTML = "已支持的应用";
     document.getElementById("title").setAttribute("data-tooltip", "下载应用 于瞬息之间 ⚡");
@@ -25,10 +25,11 @@ if (/zh-CN|zh-cn|zh-Hans|zh-hans|cn/i.test(userLang)) {
 else if (/zh-TW|zh-HK|zh-tw|zh-hk|zh-Hant|zh-hant|tw|hk/i.test(userLang)) {
     // detect browser langauge, traditional chinese only
     document.write("<style>.lato { font-family: 'Lato', --apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'PingFang TC', 'Hiragino Sans CNS', 'Microsoft JhengHei', 'Helvetica Neue', sans-serif; }</style>");
-    document.title = "ADL - 下載應用 於瞬息之間";
+    document.title = "ADL - 下載應用 就於轉眼間";
     document.getElementById("docs").innerHTML = "閱覽文檔";
+    document.getElementById("github").innerHTML = "GitHub 專頁";
     document.getElementById("blog").innerHTML = "惶心的部落格";
-    document.getElementById("title").setAttribute("data-tooltip", "下載應用 於瞬息之間 ⚡");
+    document.getElementById("title").setAttribute("data-tooltip", "下載應用 就於轉眼間 ⚡");
     document.getElementById("howto").setAttribute("data-tooltip", "閱覽文檔");
     document.getElementById("supported").innerHTML = "已支援的應用";
     document.getElementById("supported").setAttribute("data-tooltip", "已支援 " + app_supported  + " 款應用");
