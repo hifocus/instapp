b0dee63ab5bdbc9b5bb9f986547fc10fd5f4ef8d
hifocus <i@focuschen.com>
2019-10-05 00:15:28 +0100

    Update on copywriting and English font family

--- assets/scripts/adl.js
+++ assets/scripts/adl.js
@@ -1,4 +1,5 @@
-var version = "Insider";  // define adl version
+var version = "1.0";  // define adl version
+var version_type = "Stable";
 var app_supported = "999"; // define number of apps supported
 
 var userLang = navigator.language || navigator.userLanguage;
@@ -9,18 +10,21 @@ if (/zh|cn|tw|hk|TW|HK/i.test(userLang)) {
 }
 if (/zh-CN|zh-cn|zh-Hans|zh-hans|cn/i.test(userLang)) {
     // detect browser langauge, simplified chinese only
-    document.write("<style>.lato { font-family: 'Lato', -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', sans-serif; }</style>");
-    document.getElementById("github").innerHTML = "GitHub 页面";
+    document.write("<style>.lato { font-family: 'Segoe UI', -apple-system, system-ui, BlinkMacSystemFont, Roboto, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', sans-serif; }</style>");
+    document.getElementById("index").innerHTML = "首页";
+    document.getElementById("github").innerHTML = "GitHub";
     document.getElementById("blog").innerHTML = "惶心的博客";
 }
 else if (/zh-TW|zh-HK|zh-tw|zh-hk|zh-Hant|zh-hant|tw|hk/i.test(userLang)) {
     // detect browser langauge, traditional chinese only
-    document.write("<style>.lato { font-family: 'Lato', --apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'PingFang TC', 'Hiragino Sans CNS', 'Microsoft JhengHei', 'Helvetica Neue', sans-serif; }</style>");
-    document.getElementById("github").innerHTML = "GitHub 專頁";
+    document.write("<style>.lato { font-family: 'Segoe UI', --apple-system, system-ui, BlinkMacSystemFont, Roboto, 'PingFang TC', 'Hiragino Sans CNS', 'Microsoft JhengHei', 'Helvetica Neue', sans-serif; }</style>");
+    document.getElementById("index").innerHTML = "主頁";
+    document.getElementById("github").innerHTML = "GitHub";
     document.getElementById("blog").innerHTML = "惶心的部落格";
 }
 else {
     // detect browser langauge, anything other than chinese will display english
-    document.write("<style>.lato { font-family: 'Lato', -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', sans-serif; }</style>");
+    // font-family from https://docs.microsoft.com/en-gb/
+    document.write("<style>.lato { font-family: 'Segoe UI', SegoeUI, 'Segoe WP', 'Helvetica Neue', Helvetica, Tahoma, Arial, sans-serif; }</style>");
 
 }
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,21 +6,21 @@ document.getElementById("howto").innerHTML = "如何使用";
 if (/zh-CN|zh-cn|zh-Hans|zh-hans|cn/i.test(userLang)) {
     document.title = 'ADL - 下载应用 于瞬息之间';
     document.getElementById("title").setAttribute("data-tooltip", "下载应用 于瞬息之间 ⚡");
-    document.getElementById("docs").innerHTML = "浏览使用文档";
+    document.getElementById("docs").innerHTML = "使用文档";
     document.getElementById("howto").setAttribute("data-tooltip", "浏览文档");
     document.getElementById("supported").innerHTML = "已支持的应用";
     document.getElementById("supported").setAttribute("data-tooltip", "已支持 " + app_supported  + " 个应用");
-    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>你的浏览器语言为 <code>" + userLang + "</code></span><br>版本: " + "<span class='grey'>" + version +"</span> | 开源于 " + github_href;
+    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>你的浏览器语言为 <code>" + userLang + "</code></span><br>版本: " + "<span class='grey'>" + version + " " + version_type +"</span> | 开源于 " + github_href;
     document.getElementById("dont").setAttribute("data-tooltip", "不要忘记 ⭐ 我们的项目");
 }
 else if (/zh-TW|zh-HK|zh-tw|zh-hk|zh-Hant|zh-hant|tw|hk/i.test(userLang)) {
     document.title = "ADL - 下載應用 就於轉眼間";
     document.getElementById("title").setAttribute("data-tooltip", "下載應用 就於轉眼間 ⚡");
     document.getElementById("howto").setAttribute("data-tooltip", "閱覽文檔");
-    document.getElementById("docs").innerHTML = "閱覽使用文檔";
+    document.getElementById("docs").innerHTML = "使用文檔";
     document.getElementById("supported").innerHTML = "已支援的應用";
     document.getElementById("supported").setAttribute("data-tooltip", "已支援 " + app_supported  + " 款應用");
-    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>你的瀏覽器語言為 <code>" + userLang + "</code></span><br>版本: " + "<span class='grey'>" + version +"</span> |  開源於 " + github_href;
+    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>你的瀏覽器語言為 <code>" + userLang + "</code></span><br>版本: " + "<span class='grey'>" + version + " " + version_type +"</span> |  開源於 " + github_href;
     document.getElementById("dont").setAttribute("data-tooltip", "不要忘記 ⭐ 我們的項目");
 }
 else {
@@ -28,6 +28,6 @@ else {
     document.getElementById("title").setAttribute("data-tooltip", "Download apps in seconds ⚡");
     document.getElementById("howto").setAttribute("data-tooltip", "Browse docs");
     document.getElementById("supported").setAttribute("data-tooltip", app_supported  + " apps are supported");
-    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>Your browser language is <code>" + userLang + "</code></span><br>Version: " + "<span class='grey'>" + version +"</span> | Open Source Project on " + github_href;
+    document.getElementById("footer").innerHTML = "<span class='smaller-fonts'>Your browser language is <code>" + userLang + "</code></span><br>Version: " + "<span class='grey'>" + version + " " + version_type + "</span> | Open Source Project on " + github_href;
     document.getElementById("dont").setAttribute("data-tooltip", "Don't forget to ⭐ our project!");
 }
