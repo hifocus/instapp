6b0aa4b3ce92e3478cbbc7ed57b358dc21a488a3
hifocus <i@focuschen.com>
2019-12-31 12:36:16 +0800

    Sync with current work progress wait upon future changes to be made

---
+++ assets/scripts/apps/vscode.js
@@ -0,0 +1,12 @@
+// Announce app name and official site
+const app_name = "Visual Studio Code";
+const app_site = "https://code.visualstudio.com";
+
+// Announce availability on different platforms
+const windows = true;
+const macos = true;
+const android = false;
+const ios = false;
+
+const windows_link = "https://update.code.visualstudio.com/latest/win32-x64-user/stable";
+const macos_link = "https://update.code.visualstudio.com/latest/darwin/stable";
