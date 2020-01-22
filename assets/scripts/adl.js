7efe587ae5f3b9729524e2f0e41634883e64e61b
hifocus <i@focuschen.com>
2020-01-22 18:25:11 +0000

    Improve code comments & remove improper use of Segoe UI font family

--- assets/scripts/adl.js
+++ assets/scripts/adl.js
@@ -3,10 +3,6 @@ var version_type = "Alpha";
 var app_supported = "999"; // define number of apps supported
 
 var userLang = navigator.language || navigator.userLanguage;
-// if (/zh*/i.test(userLang)) {
-// adjustment for non-standard userlangauges
-// var userLang = "zh-CN";
-// }
 
 var github_href = "<a class='tooltip' target='_blank' id='dont' href='https://github.com/hifocus/adl'>GitHub</a>";
 
@@ -36,14 +32,14 @@ if (/zh|cn|tw|hk|TW|HK/i.test(userLang)) {
 }
 if (/zh-CN|zh-cn|zh-Hans|zh-hans|cn/i.test(userLang)) {
     // detect browser langauge, simplified chinese only
-    document.write("<style>.body { font-family: 'Segoe UI', -apple-system, system-ui, BlinkMacSystemFont, Roboto, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', sans-serif; }</style>");
+    document.write("<style>.body { font-family: -apple-system, system-ui, BlinkMacSystemFont, Roboto, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', sans-serif; }</style>");
     document.getElementById("index").innerHTML = "首页";
     document.getElementById("github").innerHTML = "GitHub";
     document.getElementById("blog").innerHTML = "惶心的博客";
 }
 else if (/zh-TW|zh-HK|zh-tw|zh-hk|zh-Hant|zh-hant|tw|hk/i.test(userLang)) {
     // detect browser langauge, traditional chinese only
-    document.write("<style>.body { font-family: 'Segoe UI', --apple-system, system-ui, BlinkMacSystemFont, Roboto, 'PingFang TC', 'Hiragino Sans CNS', 'Microsoft JhengHei', 'Helvetica Neue', sans-serif; }</style>");
+    document.write("<style>.body { font-family: --apple-system, system-ui, BlinkMacSystemFont, Roboto, 'PingFang TC', 'Hiragino Sans CNS', 'Microsoft JhengHei', 'Helvetica Neue', sans-serif; }</style>");
     document.getElementById("index").innerHTML = "主頁";
     document.getElementById("github").innerHTML = "GitHub";
     document.getElementById("blog").innerHTML = "惶心的部落格";
@@ -54,7 +50,7 @@ else if (/zh*/i.test(userLang)) {
 }
 
 else {
-    // detect browser langauge, anything other than chinese will display english
+    // detect browser langauge, any langauges other than chinese will display english
     // font-family from https://docs.microsoft.com/en-gb/
     document.write("<style>.body { font-family: 'Segoe UI', SegoeUI, 'Segoe WP', 'Helvetica Neue', Helvetica, Tahoma, Arial, sans-serif; }</style>");
 }
