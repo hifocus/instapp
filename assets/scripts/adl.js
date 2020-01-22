02c84f01d6bfe72c015519f8d6579c3fe478b304
hifocus <i@focuschen.com>
2020-01-22 18:20:55 +0000

    Improve the logic of identifying non-standard userlang & use js function to reduce code repetition

--- assets/scripts/adl.js
+++ assets/scripts/adl.js
@@ -3,13 +3,35 @@ var version_type = "Alpha";
 var app_supported = "999"; // define number of apps supported
 
 var userLang = navigator.language || navigator.userLanguage;
-if (/zh*/i.test(userLang)) {
+// if (/zh*/i.test(userLang)) {
 // adjustment for non-standard userlangauges
-var userLang = "zh-CN";
+// var userLang = "zh-CN";
+// }
+
+var github_href = "<a class='tooltip' target='_blank' id='dont' href='https://github.com/hifocus/adl'>GitHub</a>";
+
+function trans(vn, lc) { // vn = variable name, lc = localised content
+    return document.getElementById(vn).innerHTML = (lc);
 }
+
+function ct(cont) { // ct = change title
+    document.title = cont;
+}
+
+function st(vn, cont) { // st = set tooltip, cont = content
+    return document.getElementById(vn).setAttribute("data-tooltip", cont);
+}
+
+function st2(vn, cont1, cont2) {
+    return document.getElementById(vn).setAttribute("data-tooltip", cont1 + app_supported + cont2);
+}
+
+const footer_innerhtml = (...args) => {
+    document.getElementById("footer").innerHTML = (args)
+};
+
+
 if (/zh|cn|tw|hk|TW|HK/i.test(userLang)) {
-    // detect browser langauge, both simplified chinese and traditional chinese
-    // place chinese translations in common here
     document.getElementById("discover").innerHTML = "更多";
 }
 if (/zh-CN|zh-cn|zh-Hans|zh-hans|cn/i.test(userLang)) {
@@ -26,9 +48,13 @@ else if (/zh-TW|zh-HK|zh-tw|zh-hk|zh-Hant|zh-hant|tw|hk/i.test(userLang)) {
     document.getElementById("github").innerHTML = "GitHub";
     document.getElementById("blog").innerHTML = "惶心的部落格";
 }
+
+else if (/zh*/i.test(userLang)) {
+    var userLang = "zh-CN"; // Default non-standard userlang to simplified chinese
+}
+
 else {
     // detect browser langauge, anything other than chinese will display english
     // font-family from https://docs.microsoft.com/en-gb/
     document.write("<style>.body { font-family: 'Segoe UI', SegoeUI, 'Segoe WP', 'Helvetica Neue', Helvetica, Tahoma, Arial, sans-serif; }</style>");
-
 }
